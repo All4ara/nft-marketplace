94e154fa94466f8ab4f8cf2438212c8df10df441
Ariel Elazarov <ariel.elazarov@yahoo.com>
2022-03-24 16:23:41 -0400

    Updated Marketplace contract to make and sell the nfts aswel as make offer on current nfts on the marketplace

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -20,8 +20,41 @@ contract Marketplace is ReentrancyGuard {
         bool sold;
     }
 
+    event Offered(
+        uint256 itemId,
+        address indexed nft,
+        uint256 tokenId,
+        uint256 price,
+        address indexed seller
+    );
+
+    // itemId -> Item
+    mapping(uint256 => Item) public items;
+
     constructor(uint256 _feePercent) {
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
+    }
+
+    function makeItem(
+        IERC721 _nft,
+        uint256 _tokenId,
+        uint256 _price
+    ) external nonReentrant {
+        require(_price > 0, "Price must be greater than zero");
+        itemCount++;
+        // transfer nft
+        _nft.transferFrom(msg.sender, address(this), _tokenId);
+        // add new item to items mapping
+        items[itemCount] = Item(
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
+        );
+        // emit Offered event
+        emit Offered(itemCount, address(_nft), _tokenId, _price, msg.sender);
     }
 }
